a41f57c41a0f04dcc4c9af645c9e9ca285694e1c
SotDousk <mechsot7@hotmail.com>
2018-05-06 21:29:27 +0300

    Smooth scrolling (upds on css and js)

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,16 +1,34 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+//// When the user scrolls down 20px from the top of the document, show the button
+//window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+//function scrollFunction() {
+//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//        document.getElementById("myBtn").style.display = "block";
+//    } else {
+//        document.getElementById("myBtn").style.display = "none";
+//    }
+//}
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+//function topFunction() {
+//    document.body.scrollTop = 0;
+//    document.documentElement.scrollTop = 0;
+//
+/*  Scroll to top when arrow up clickec BEGIN */
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+	$('#back2Top').fadeIn();
+    } else {
+	$('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+	return false;
+    });
+
+});
+/* Scroll to top when arrow up clicked END */
